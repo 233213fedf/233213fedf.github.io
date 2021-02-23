471b2b84ffb244b1fe66fdf5c0e0730b33105f5e
233213fedf <z233213fedf@163.com>
2021-02-23 18:41:29 +0800

    Site updated: 2021-02-23 18:41:24

--- live2d/js/message.js
+++ live2d/js/message.js
@@ -61,10 +61,10 @@ initTips();
 (function (){
     var text;
     if(document.referrer !== ''){
-     //   var referrer = document.createElement('a');
-   //     referrer.href = document.referrer;
-    //    text = '嗨！来自 <span style="color:#0099cc;">' + referrer.hostname + '</span> 的朋友！';
-     //   var domain = referrer.hostname.split('.')[1];
+        var referrer = document.createElement('a');
+        referrer.href = document.referrer;
+        text = '嗨！来自 <span style="color:#0099cc;">' + referrer.hostname + '</span> 的朋友！';
+        var domain = referrer.hostname.split('.')[1];
         if (domain == 'baidu') {
             text = '嗨！ 来自 百度搜索 的朋友！<br>欢迎访问<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
         }else if (domain == 'so') {
